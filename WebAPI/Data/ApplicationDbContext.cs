d49b8472cf797b0e7a4b236eb590aadd2e088f97
Martin Kohout <Martin.Kohout@365.pslib.cz>
2020-10-01 13:40:59 +0200

    Seed Db

--- WebAPI/Data/ApplicationDbContext.cs
+++ WebAPI/Data/ApplicationDbContext.cs
@@ -17,5 +17,22 @@ namespace WebAPI.Data
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<QuoteTag> QuoteTags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Quote>().HasData(new Quote { Id = 1, Text = "Blablabla very smart quote :)" });
+            modelBuilder.Entity<Quote>().HasData(new Quote { Id = 2, Text = "Blablabla very another smart quote :)" });
+            modelBuilder.Entity<Quote>().HasData(new Quote { Id = 3, Text = "Kdo pozdě chodí, sám sobě škodí." });
+
+            modelBuilder.Entity<Tag>().HasData(new Tag { Id = 1, Name = "Kohout", Category = Category.Author });
+            modelBuilder.Entity<Tag>().HasData(new Tag { Id = 2, Name = "Pepa Autor", Category = Category.Author });
+            modelBuilder.Entity<Tag>().HasData(new Tag { Id = 3, Name = "České příslový", Category = Category.Genre });
+
+            modelBuilder.Entity<QuoteTag>().HasData(new QuoteTag { Id = 1, QuoteId = 1, TagId = 1 });
+            modelBuilder.Entity<QuoteTag>().HasData(new QuoteTag { Id = 2, QuoteId = 2, TagId = 2});
+            modelBuilder.Entity<QuoteTag>().HasData(new QuoteTag { Id = 3, QuoteId = 3, TagId = 3});
+        }
     }
 }
---
+++ WebAPI/Data/Migrations/20201001114049_SeedDb.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Data.Migrations
+{
+    public partial class SeedDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "Id", "Text" },
+                values: new object[,]
+                {
+                    { 1, "Blablabla very smart quote :)" },
+                    { 2, "Blablabla very another smart quote :)" },
+                    { 3, "Kdo pozdě chodí, sám sobě škodí." }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Category", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, "Kohout" },
+                    { 2, 0, "Pepa Autor" },
+                    { 3, 1, "České příslový" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "QuoteTags",
+                columns: new[] { "Id", "QuoteId", "TagId" },
+                values: new object[] { 1, 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "QuoteTags",
+                columns: new[] { "Id", "QuoteId", "TagId" },
+                values: new object[] { 2, 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "QuoteTags",
+                columns: new[] { "Id", "QuoteId", "TagId" },
+                values: new object[] { 3, 3, 3 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "QuoteTags",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "QuoteTags",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "QuoteTags",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
